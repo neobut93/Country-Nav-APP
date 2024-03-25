2016e189d086ec54c0ab7872d1f024ddc16049fe
vadim <neobut93@gmail.com>
2024-03-24 18:37:26 -0700

    added app version from BuildConfig

--- app/src/main/java/com/kodeco/android/countryinfo/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/screens/about/AboutScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.kodeco.android.countryinfo.BuildConfig
 import com.kodeco.android.countryinfo.R
 
 @Composable
@@ -61,7 +62,7 @@ fun AboutScreen(
                 lineHeight = 40.sp
             )
             Text(
-                text = stringResource(R.string.app_version),
+                text = stringResource(R.string.app_version, BuildConfig.VERSION_NAME),
                 fontSize = 30.sp,
                 modifier = Modifier
                     .weight(1f)
